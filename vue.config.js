7c34cd19603fdf08b9135bfbe1fb82e9c7629fbe
tangrenyue <724562793@qq.com>
2019-05-06 09:53:41 +0800

    config-mock注册

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 module.exports = {
+  configureWebpack: {
+    devServer: {
+      //MOCK接口编写的地方
+      //每次做更改这个配置文件的时候，都必须重启项目才会生效
+      before(app) {
+        //用户信息池
+        let userpoor = [{
+            username: 'xiaod',
+            password: '123456'
+          },
+          {
+            username: 'tim',
+            password: '123456'
+          }
+        ]
+        //注册接口
+        app.get('/api/register', (req, res) => {
+          const {
+            username,
+            password
+          } = req.query
+          const userlength = userpoor.filter(v => v.username == username).length
+          if (userlength > 0) {
+            res.json({
+              success: false,
+              message: '用户名已存在'
+            })
+          } else {
+            res.json({
+              success: true,
+              message: '注册成功'
+            })
+          }
+        })
+      }
+    }
+  },
   css: {
     loaderOptions: {
       stylus: {
