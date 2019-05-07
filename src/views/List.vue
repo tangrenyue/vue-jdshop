fac00f1ed1691be97bc3b958cbc8bb659637a705
tangrenyue <724562793@qq.com>
2019-05-07 10:56:11 +0800

    分类页rpanel-mock

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,17 +1,154 @@
 <template>
-    <div>
-        分类页
-    </div>
+  <div class="panelsbox">
+    <cube-scroll class="leftpanels">
+      <ul>
+        <li
+          v-for="(list,index) in tabslabel"
+          @click="selectlist(index)"
+          :class="list.active?'active':''"
+          :key="index"
+        >{{list.label}}</li>
+      </ul>
+    </cube-scroll>
+    <cube-scroll class="rightpanels">
+      <ul>
+        <li v-for="(tag,index) in tags" :key="index">
+          <img :src="tag.image" alt>
+          <p>{{tag.label}}</p>
+        </li>
+      </ul>
+    </cube-scroll>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      tags: [],
+      tabslabel: [
+        {
+          label: "热门推荐",
+          active: true
+        },
+        {
+          label: "手机数码",
+          active: false
+        },
+        {
+          label: "家用电器",
+          active: false
+        },
+        {
+          label: "汽车生活",
+          active: false
+        },
+        {
+          label: "电脑办公",
+          active: false
+        },
+        {
+          label: "图书音像",
+          active: false
+        },
+        {
+          label: "美妆护肤",
+          active: false
+        },
+        {
+          label: "运动户外",
+          active: false
+        },
+        {
+          label: "生活旅行",
+          active: false
+        },
+        {
+          label: "食品生鲜",
+          active: false
+        },
+        {
+          label: "玩具乐器",
+          active: false
+        },
+        {
+          label: "家具家装",
+          active: false
+        }
+      ]
+    };
+  },
+  methods: {
+    //点击左侧分类-分类数据
+    selectlist(index) {
+      this.tabslabel.forEach((val, ind) => {
+        if (index == ind) {
+          val.active = true;
+        } else {
+          val.active = false;
+        }
+      });
+      this.getclassify(index);
+    },
+    //获取分类
+    async getclassify(index) {
+      const result = await this.$http.get("/api/classify", {
+        params: { type: index }
+      });
+      this.tags = result.data;
+    }
+  },
+  created() {
+    //获取默认的分类数据
+    this.getclassify(0);
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
+.panelsbox {
+  display: flex;
 
+  .leftpanels {
+    width: 30%;
+
+    li {
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 1px solid #fff;
+      color: #333;
+      background: #f8f8f8;
+      font-size: 14px;
+    }
+
+    .active {
+      background: #fff;
+      color: #e93b3d;
+    }
+  }
+
+  .rightpanels {
+    width: 70%;
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+
+      li {
+        width: 50%;
+        justify-content: center;
+        align-items: center;
+        font-size: 15px;
+
+        img {
+          width: 80px;
+          height: 80px;
+        }
+      }
+    }
+  }
+}
 </style>
+
 
 
